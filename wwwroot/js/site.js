101b43e7cb556c6fa5cdb7cf66942af44d97373d
Abhijit Jana <mca_abhijit@yahoo.co.in>
2017-12-17 20:15:56 +0530

    Init

---
+++ wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿// Write your JavaScript code.
+function ThisFuncton(id, person) {
+console.log('test1');
+console.log('');
+
+}
+
+// This function rocks
+
+// This function rocks
+
+// This function rocks
+
+function name(){
+    return value
+}
